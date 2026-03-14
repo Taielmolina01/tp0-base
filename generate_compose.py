3a0f4b48450ace9c94c02a8f12a9f7d7cdc5f593
tamolina01 <tamolina@fi.uba.ar>
2026-03-13 23:24:30 -0300

    first approach to solution of 1st exercise

---
+++ generate_compose.py
@@ -0,0 +1,69 @@
+import sys 
+
+EXPECTED_AMOUNT_OF_ARGUMENTS = 2
+MSG_WRONG_AMOUNT_OF_ARGUMENTS = f"""Bad amount of arguments. 
+        This program must be called with {EXPECTED_AMOUNT_OF_ARGUMENTS} arguments.
+        Example: 'python3 generate_compose.py ${{output_file}} ${{amount_of_clients}}'"""
+
+
+def main():
+    if len(sys.argv) != EXPECTED_AMOUNT_OF_ARGUMENTS + 1:
+        print(MSG_WRONG_AMOUNT_OF_ARGUMENTS)
+        return 
+
+    output_file = sys.argv[1]
+    amount_of_clients = int(sys.argv[2])
+
+    with open(output_file, 'w') as f:
+        for line in get_server_lines():
+            f.write(line)
+        for i in range (1, amount_of_clients + 1):
+            for line in get_client_lines(i):
+                f.write(line)
+        for line in get_network_lines():
+            f.write(line)        
+
+def get_server_lines() -> str:
+    return [
+        "name: tp0\n",
+        "services:\n",
+        "  server:\n",
+        "    container_name: server\n",
+        "    image: server:latest\n",
+        "    entrypoint: python3 /main.py\n",
+        "    environment:\n",
+        "      - PYTHONUNBUFFERED=1\n",
+        "      - LOGGING_LEVEL=DEBUG\n",
+        "    networks:\n",
+        "      - testing_net\n\n"
+    ]
+
+def get_client_lines(
+    client_number: int
+    ) -> str:
+    return [
+        f"  client{client_number}:\n",
+        f"    container_name: client{client_number}\n",
+        "    image: client:latest\n",
+        "    entrypoint: /client\n",
+        "    environment:\n",
+        f"      - CLI_ID={client_number}\n",
+        "      - CLI_LOG_LEVEL=DEBUG\n",
+        "    networks:\n",
+        "      - testing_net\n",
+        "    depends_on:\n",
+        "      - server\n\n"
+    ]
+
+def get_network_lines() -> str:
+    return [
+        "networks:\n",
+        "  testing_net:\n",
+        "    ipam:\n",
+        "      driver: default\n",
+        "      config:\n",
+        "        - subnet: 172.25.125.0/24\n"
+    ]
+
+if __name__ == '__main__':
+    main() 
