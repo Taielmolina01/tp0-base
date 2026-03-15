8a40d6df6cf96e00062aa9dc5e14f8f39bac0e1d
tamolina01 <tamolina@fi.uba.ar>
2026-03-15 15:41:04 -0300

    add: timeout on nc command

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -4,7 +4,7 @@ apk add --no-cache netcat-openbsd &&
 message="hola"
 success_message="action: test_echo_server | result: success"
 fail_message="action: test_echo_server | result: fail"
-echo_response=$(echo "$message" | nc server 12345) &&
+echo_response=$(echo "$message" | nc -w 2 server 12345) &&
 if [ "$echo_response" = "$message" ]; then 
     echo "$success_message" 
 else
