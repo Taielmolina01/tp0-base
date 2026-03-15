979660f24ff18e358fda0f22d5c5522f4f3193e9
tamolina01 <tamolina@fi.uba.ar>
2026-03-15 15:01:22 -0300

    feat: add ej3 solution

---
+++ validar-echo-server.sh
@@ -0,0 +1,13 @@
+testing_network="tp0_testing_net"
+docker run --rm --network "$testing_network" alpine sh -c '
+apk add --no-cache netcat-openbsd &&
+message="hola"
+success_message="action: test_echo_server | result: success"
+fail_message="action: test_echo_server | result: fail"
+echo_response=$(echo "$message" | nc server 12345) &&
+if [ "$echo_response" = "$message" ]; then 
+    echo "$success_message" 
+else
+    echo "$fail_message"
+fi
+'
