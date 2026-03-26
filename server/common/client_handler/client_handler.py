92678ea661cfd018aba48885dc77783787a91964
tamolina01 <tamolina@fi.uba.ar>
2026-03-26 10:20:53 -0300

    some fixes

--- server/common/client_handler/client_handler.py
+++ server/common/client_handler/client_handler.py
@@ -3,7 +3,7 @@ from common.protocol.exceptions.eoc_exception import EndOfCommunicationException
 from common.protocol.exceptions.bad_amount_fields_bet import (
     BadAmountOfFieldsInBet,
 )
-from common.utils import store_bets, load_bets, has_won
+from common.utils import load_bets, has_won
 from common.lottery.lottery import Lottery
 import logging
 from threading import Barrier
@@ -73,7 +73,7 @@ class ClientHandler:
         self.__protocol.send_results_to_agency(
             [
                 int(bet.document)
-                for bet in self.server_monitor.load_bets_safe()
+                for bet in load_bets()
                 if bet.agency == self.id and has_won(bet)
             ]
         )
--- server/common/server_monitor/server_monitor.py
+++ server/common/server_monitor/server_monitor.py
@@ -1,4 +1,4 @@
-from common.utils import store_bets, load_bets, has_won
+from common.utils import store_bets
 from threading import Lock
 
 
@@ -9,7 +9,3 @@ class ServerMonitor:
     def store_bets_safe(self, bets):
         with self.lock:
             store_bets(bets)
-
-    def load_bets_safe(self):
-        with self.lock:
-            return load_bets()
