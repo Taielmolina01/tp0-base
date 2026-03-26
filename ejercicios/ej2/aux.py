e8fd911eacdb0c09e2e62a07ad40093faf485605
tamolina01 <tamolina@fi.uba.ar>
2026-03-26 08:25:48 -0300

    format w black and deleting volumes lines

--- ejercicios/ej2/aux.py
+++ ejercicios/ej2/aux.py
@@ -1,4 +1,4 @@
-def get_server_lines() -> [str]:
+def get_server_lines() -> list[str]:
     return [
         "name: tp0\n",
         "services:\n",
@@ -11,12 +11,11 @@ def get_server_lines() -> [str]:
         "    networks:\n",
         "      - testing_net\n",
         "    volumes:\n",
-        "      - ./server/config.ini:/config/config.ini\n\n"
+        "      - ./server/config.ini:/config/config.ini\n\n",
     ]
 
-def get_client_lines(
-    client_number: int
-    ) -> [str]:
+
+def get_client_lines(client_number: int) -> list[str]:
     return [
         f"  client{client_number}:\n",
         f"    container_name: client{client_number}\n",
@@ -29,34 +28,29 @@ def get_client_lines(
         "    volumes:\n",
         "      - ./client/config.yaml:/config/config.yaml\n",
         "    depends_on:\n",
-        "      - server\n\n"
+        "      - server\n\n",
     ]
 
-def get_network_lines() -> [str]:
+
+def get_network_lines() -> list[str]:
     return [
         "networks:\n",
         "  testing_net:\n",
         "    ipam:\n",
         "      driver: default\n",
         "      config:\n",
-        "        - subnet: 172.25.125.0/24\n\n"
+        "        - subnet: 172.25.125.0/24\n\n",
     ]
 
-def get_volumes_lines() -> [str]:
-    return [
-        "volumes:\n",
-        "  server_config:\n",
-        "  client_config:\n"
-    ]
+
+
 
 def generate_compose_file(output_file: str, amount_of_clients: int):
-    with open(output_file, 'w') as f:
+    with open(output_file, "w") as f:
         for line in get_server_lines():
             f.write(line)
-        for i in range (1, amount_of_clients + 1):
+        for i in range(1, amount_of_clients + 1):
             for line in get_client_lines(i):
                 f.write(line)
         for line in get_network_lines():
-            f.write(line)        
-        for line in get_volumes_lines():
             f.write(line)
--- ejercicios/ej2/generate_compose.py
+++ ejercicios/ej2/generate_compose.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 from aux import generate_compose_file
 
 EXPECTED_AMOUNT_OF_ARGUMENTS = 2
@@ -6,15 +6,17 @@ MSG_WRONG_AMOUNT_OF_ARGUMENTS = f"""Bad amount of arguments.
         This program must be called with {EXPECTED_AMOUNT_OF_ARGUMENTS} arguments.
         Example: 'python3 generate_compose.py ${{output_file}} ${{amount_of_clients}}'"""
 
+
 def main():
     if len(sys.argv) != EXPECTED_AMOUNT_OF_ARGUMENTS + 1:
         print(MSG_WRONG_AMOUNT_OF_ARGUMENTS)
-        return 
+        return
 
     output_file = sys.argv[1]
     amount_of_clients = int(sys.argv[2])
 
     generate_compose_file(output_file, amount_of_clients)
 
-if __name__ == '__main__':
-    main() 
+
+if __name__ == "__main__":
+    main()
