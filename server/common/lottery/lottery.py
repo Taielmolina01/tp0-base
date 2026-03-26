ed004ba2e5fd0e073be116baafc4a669a3702f1d
tamolina01 <tamolina@fi.uba.ar>
2026-03-26 01:41:50 -0300

    one more fix

--- server/common/lottery/lottery.py
+++ server/common/lottery/lottery.py
@@ -8,9 +8,9 @@ class Lottery:
         self.amount_of_agencies = amount_of_agencies
         self.amount_of_agencies_ready = 0
 
-    def check_agency_as_finished(self):
+    def _check_agency_as_finished(self):
         self.amount_of_agencies_ready += 1
         return True
     
-    def check_finished(self):
+    def _check_finished(self):
         return self.amount_of_agencies_ready == self.amount_of_agencies
